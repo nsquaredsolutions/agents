558cceef040c131798a4c77c6c55a39748ada414
drneil <neil@roodyn.com>
2025-02-04 18:39:13 +1100

    Add Settings UserControl for CommandWithSettings example

--- Samples/CommandWithSettings/Command.cs
+++ Samples/CommandWithSettings/Command.cs
@@ -3,16 +3,19 @@ namespace SimpleCommand;
 
 public class Command : IAgentCommand
 {
+    private static SimpleViewModel ViewModel => new();
     public string Name => "SimpleCommand";
 
-    public bool HasSettings => false;
+    public bool HasSettings => true;
     public AgentCommandType CommandType => AgentCommandType.KeywordLocal;
+
+    public object SettingsControl => new SettingsControl(ViewModel);
 
     Task<string?> IAgentCommand.Perform(string commandRequest, IAgentAnimations? animations)
     {
         if (commandRequest.Contains("simple", StringComparison.CurrentCultureIgnoreCase))
         {
-            return Task.FromResult<string?>("I am doing a simple command!");
+            return Task.FromResult<string?>($"hello {ViewModel.Name} I am doing a simple command!");
         }
         return Task.FromResult<string?>(null);
     }
